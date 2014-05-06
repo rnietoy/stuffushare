47efdcd721bef2ccc2aa2401f1cc52edcf59af17
angelga <azmavet@gmail.com>
2014-05-05 21:04:26 -0700

    Static files and template support.

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic.base import View
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-class HelloWorldView(View):
-    def get(self, request):
-        return HttpResponse("Welcome to StuffUShare!")
+class HomeView(TemplateView):
+    template_name="base.html"
+
+    def get_context_data(self, **kwargs):
+        return {'user_name': "Angel"}
--- stuffushare/urls.py
+++ stuffushare/urls.py
@@ -1,14 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-
-from main.views import HelloWorldView
+from main.views import HomeView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', HelloWorldView.as_view(), name='home'),
+    url(r'^$', HomeView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
