f1371d6cbc4bb214defd131fb65d443bb355feea
angelga <azmavet@gmail.com>
2014-05-05 21:30:54 -0700

    Empty homepage with template inheritance.

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 class HomeView(TemplateView):
-    template_name="base.html"
+    template_name="home.html"
 
     def get_context_data(self, **kwargs):
         return {'user_name': "Angel"}
--- stuffushare/settings.py
+++ stuffushare/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
+    'video',
+    'user',
 )
 
 MIDDLEWARE_CLASSES = (
